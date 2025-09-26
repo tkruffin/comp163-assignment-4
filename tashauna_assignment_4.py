2c2d24aa1cf5590b7035a8ad09e722519ddbcd7d
tkruffin <tkruffin@aggies.ncat.edu>
2025-09-26 15:46:41 -0400

    Add files via upload

---
+++ tashauna_assignment_4.py
@@ -0,0 +1,7 @@
+student_name = "Tashauna Ruffin"
+current_gpa = 2.8
+study_hours = 26
+social_points = 10
+stress_level = 65 
+
+print(f"Welocme! \nName:{student_name} \nCurrent GPA: {current_gpa} \nStudy hours:{study_hours} \nSocial Points: {social_points} \nStress Level:{stress_level}")
